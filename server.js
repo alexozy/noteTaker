056ea0a94e97ceac610ece16f506e7c9fa8d7636
Ozanne Alexander <alexanoz@outlook.com>
2023-01-30 20:13:15 -0500

    Add heroku

--- server.js
+++ server.js
@@ -3,28 +3,41 @@ const express = require('express');
 const path = require('path');
 const fs = require("fs");
 
-// This will tell node we are creating an 'express' server
-// START express.js; later you can chain methods to this variable easily
-const app = express();
+// require notes
+const notes = require('./db/db.json');
 
 // make the server listen 
 const PORT = process.env.PORT || 3001;
 //  a port = think coordinates. it gives an exact location
 
+// START express.js; later you can chain methods to this variable easily
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// GET HTML ROUTES | app.get() needs 2 args: 1 -> route 2 -> callback function executed every time
+
+// app.get() needs 2 args: 1 -> route 2 -> callback function executed every time
+
+
 // route to notes.html page
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname,'/public/notes.html'))
 });
 
+
 // GET  API Routes / route to index.html page
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
+
+
+// How it listens to the connection:
+
+app.listen(PORT, () => {
+    console.log(`Current Port : ${PORT}`)
+})
+
 // from class notes this returns JSON instead of strings
 // app.get('/api', (req, res) => res.json(termData));
 
@@ -33,13 +46,6 @@ app.get('/', (req,res) => {
 //         console.log(err)
 //     })
 // })
-
-// How it listens to the connection:
-
-app.listen(PORT, () => {
-    console.log(`Current Port : ${PORT}`)
-})
-
 
 
 
