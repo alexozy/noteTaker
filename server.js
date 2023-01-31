81fd0dc9d4887db1cdef7b707ecb7b010352776c
Ozanne Alexander <alexanoz@outlook.com>
2023-01-30 19:50:44 -0500

    cleaned up redundant code

--- server.js
+++ server.js
@@ -12,28 +12,34 @@ const PORT = process.env.PORT || 3001;
 //  a port = think coordinates. it gives an exact location
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 // GET HTML ROUTES | app.get() needs 2 args: 1 -> route 2 -> callback function executed every time
 // route to notes.html page
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname,'/public/notes.html'))
 });
 
-// route to index.html page
+// GET  API Routes / route to index.html page
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
+// from class notes this returns JSON instead of strings
+// app.get('/api', (req, res) => res.json(termData));
 
-// GET  API Routes 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
-
+// app.get('/dennis', (req, res) => {
+//     fs.readFile('./db/db.json', 'utf8', (err) => {
+//         console.log(err)
+//     })
+// })
 
 // How it listens to the connection:
 
 app.listen(PORT, () => {
     console.log(`Current Port : ${PORT}`)
 })
+
 
 
 
